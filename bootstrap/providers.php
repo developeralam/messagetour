70a8f1a9562a504fe946cf87eae9e2a3ff36554c
Md Emon <mail.emon.bd123@gmail.com>
2025-10-05 15:25:23 +0600

    check

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\GlobalSettingsServiceProvider::class,
-    App\Providers\GlobalSettingsServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
